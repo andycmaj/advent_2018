e3063f720aa13cf541075677400d3e5b2ee82316
Andy Cunningham <andycunn@gmail.com>
2018-12-07 00:34:38 -0800

    finish day 1

--- day01/src/lib.rs
+++ day01/src/lib.rs
@@ -1,16 +1,35 @@
-use std::fs;
+use std::collections::HashSet;
 use std::env;
+use std::fs;
 
 #[no_mangle]
 pub fn get_answer() -> String {
     let args: Vec<String> = env::args().collect();
 
     let contents = fs::read_to_string(&args[3]).expect("Something went wrong reading the file");
-
-    let sum: i32 = contents
-        .split('\n')
+    let deltas: Vec<i32> = contents
+        .split_whitespace()
         .map(|x| x.parse::<i32>().unwrap())
-        .sum();
+        .collect();
 
-    sum.to_string()
+    // Part 1
+    let sum: i32 = deltas.iter().sum();
+
+    println!("part 1: {}", sum);
+
+    let looper = deltas.into_iter().cycle();
+
+    let mut seen = HashSet::new();
+    seen.insert(0);
+
+    let mut frequency = 0;
+    for delta in looper {
+        frequency += delta;
+        if seen.contains(&frequency) {
+            break;
+        }
+        seen.insert(frequency);
+    }
+
+    return frequency.to_string();
 }
